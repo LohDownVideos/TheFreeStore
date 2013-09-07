8e04b5faea291d4888bf13c9b1366945376ecddb
Michael Blais Younkin <younkin.michael@gmail.com>
2013-09-07 18:18:43 -0400

    Move /api/food to /api/events

--- server/app.js
+++ server/app.js
@@ -3,36 +3,51 @@ var express = require('express');
 var config = require('./config');
 
 var app = express();
-var foodData = generateFood();
+var events = generateEvents();
 
 app.use(express.static(__dirname + '/' + config.webserver.static));
 app.use(express.bodyParser());
 
-app.get('/api/food', function(req, res) {
+app.get('/api/events', function(req, res) {
   res.json({
-    "food": foodData
+    "events": events
   });
 });
 
-app.put('/api/food', function(req, res) {
-  foodData.push(req.body);
+app.put('/api/events', function(req, res) {
+  events.push(req.body);
 });
 
 app.listen(config.webserver.port, config.webserver.host);
 
-function generateFood() {
-  var food = [];
+function generateEvents() {
+  var events = [];
   for (var i = 0; i < 100; i++) {
-    food.push({
-      food: 'pizza' + i,
-      instructions: 'Requires a UMD Student ID',
-      sponsor: 'PennApps',
+    events.push({
       latitude: i,
       longitude: i,
-      address: '713 Chessie Crossing Way, Woodbine, MD 21797',
-      startTime: new Date(i),
-      endTime: new Date(i + 1000)
+      room: 'Lounge ' + i,
+      name: 'Event ' + i,
+      sponsor: 'Sponsor ' + i,
+      address: '71' + i + ' Chessie Crossing Way',
+      city: 'Woodbine',
+      state: 'Maryland',
+      zipcode: 21797,
+      food: [
+        {
+          name: 'PepperoniPizza',
+          generalAmount: 'more than 50',
+          vendor: 'unknown',
+          quality: 100
+      }, {
+          name: 'Lobster',
+          generalAmount: 'about 10',
+          vendor: 'Phillips Buffet',
+          quality: 100
+      }],
+      instructions: 'Swipe your Student ID card',
+      restrictions: 'ID card required'
     });
   }
-  return food;
+  return events;
 }
